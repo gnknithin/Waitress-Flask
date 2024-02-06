8b9699d1e6be4981a150871428f136431012dfe9
Nithin <gnknithin@gmail.com>
2024-02-06 19:45:24 +0530

    misc (help) : added comments

--- tests/load/test_health_handler.py
+++ tests/load/test_health_handler.py
@@ -1,4 +1,6 @@
 # https://progressstory.com/tech/python/stress-test-with-locust-python-application/
+# make run-dev-server
+# locust -f tests/load/test_health_handler.py
 from locust import HttpUser, task
 class TestHealthHandler(HttpUser):
 
